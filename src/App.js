5f3dda9c6a7ceb9dd6c1c7895dd3f67e3be8ea78
Priyanka Malviya <priyankamalviya@Priyankas-MacBook-Pro.local>
2017-04-16 09:08:07 -0700

    added search term feature

--- src/App.js
+++ src/App.js
@@ -19,6 +19,19 @@ const list = [
   }
 ];
 
+
+//define higher order function isSearched outside of class Component
+
+/* ES5
+function isSearched(searchTerm){
+  return function(item){
+    return !searchTerm || item.title.toLowerCase().includes(searchTerm);
+  }
+} */
+
+/*ES6*/
+const isSearched = (searchTerm) => (item) => !searchTerm || item.title.toLowerCase().includes(searchTerm.toLowerCase());
+
 class App extends Component {
 
   constructor(props){
@@ -26,10 +39,14 @@ class App extends Component {
 
     this.state ={
       list,
+      searchTerm: ''
     };
 
     //bind onDismiss class method in the constructor
     this.onDismiss = this.onDismiss.bind(this);
+
+    //bind onSearchChange class method within the constructor
+    this.onSearchChange = this.onSearchChange.bind(this);
   }
 
   onDismiss(id){
@@ -38,15 +55,25 @@ class App extends Component {
     }
 
     const updatedList = this.state.list.filter(isNotId);
+    this.setState({list: updatedList});
   }
+
+  onSearchChange(event){
+    this.setState({searchTerm: event.target.value});
+  }
+
+
 
   render() {
     const hello = 'Welcome to React Hacker News App';
     return (
       <div className="App">
         <h1>Welcome to React Page</h1>
+
+        {/* define onChange callback function for the input field to hook synthetic event */}
+        <form><input type="text" onChange={this.onSearchChange}/></form>
         {
-          this.state.list.map(item =>
+          this.state.list.filter(isSearched(this.state.searchTerm)).map(item =>
               <div key={item.objectID}>
               <span><a href={item.url} >{item.title}</a></span>
               <span>{item.author}</span>
